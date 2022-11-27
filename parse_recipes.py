255cae83fc675c7101b6617aca362328959f06a4
pasavec008 <m.adamovic999@gmail.com>
2022-11-27 09:23:50 +0100

    Shift main filter to first process for speed

--- parse_recipes.py
+++ parse_recipes.py
@@ -22,17 +22,17 @@ def is_recipe(raw_recipe_dict, wl):
     title_blacklist = 'user|talk|[0-9]'
     if re.search(title_blacklist, raw_recipe_dict['title'].lower()):
         return 0
-    checks = [0, 0]
+    check = 0
     plainlist_case = 0
     for i in raw_recipe_dict['raw_text'].splitlines():
         if plainlist_case:
             if not re.search('\*', i):
                 plainlist_case = 0
                 delete_duplicates(raw_recipe_dict)
-                checks[0] = 1
+                check = 1
             else:
                 [raw_recipe_dict['ingredients'].append(wl.lemmatize(ingredient).lower()) for ingredient in re.findall("(?!{})\\b\w+[\w\-']+\w".format(blacklist), i.lower())]
-            continue
+                continue
         if re.search('main_ingredient', i):
             raw_recipe_dict['ingredients'] = list(dict.fromkeys([wl.lemmatize(ingredient).lower() for ingredient in re.findall("(?!{})\\b\w+[\w\-']+\w".format(blacklist), i.lower())]))
             
@@ -44,18 +44,16 @@ def is_recipe(raw_recipe_dict, wl):
                 raw_recipe_dict['ingredients'].pop(0)
                 continue
             delete_duplicates(raw_recipe_dict)
-            checks[0] = 1
-        if re.search('salt', i):
-            checks[1] = 1
-        if not 0 in checks:
+            check = 1
+        if check:
             #print(recipe_dict)
             del raw_recipe_dict['raw_text']
             return raw_recipe_dict
     return 0
 
 def parse_selected_recipes(recipes_to_parse, parsed_recipes_queue):
-    #x = 0
-    #diff = 0
+    x = 0
+    diff = 0
     wl = WordNetLemmatizer()
     while 1:
         read_recipe = recipes_to_parse.get()
@@ -64,17 +62,20 @@ def parse_selected_recipes(recipes_to_parse, parsed_recipes_queue):
             break
         parsed_maybe_recipe = is_recipe(read_recipe, wl)
         if parsed_maybe_recipe:
-            parsed_recipes_queue.put(parsed_maybe_recipe)
-            #x += 1
-            #print(x, diff)
-            #diff = 0
+            parsed_recipes_queue.put_nowait(parsed_maybe_recipe)
+            x += 1
+            print(x, diff)
+            diff = 0
     print('Done parse')
-    parsed_recipes_queue.put(0)
+    parsed_recipes_queue.put_nowait(0)
     return
 
 def read_raw(wiki_file_name, recipes_to_parse):
     source_file = bz2.BZ2File(wiki_file_name, 'r')
     mode = 0
+    ingredients = 0
+    plainlist = 0
+    salt = 0
     for line in source_file:
         decoded_line = line.decode('utf-8')
         #finding title
@@ -89,14 +90,28 @@ def read_raw(wiki_file_name, recipes_to_parse):
 
         #finding end of text or printing
         if mode == 2:
-            if re.search('</text>', decoded_line):
-                mode = 0
-                recipes_to_parse.put(recipe_dict_raw)
-            else:
+            if plainlist:
+                if not re.search('\*', decoded_line):
+                    plainlist = 0
+                    recipe_dict_raw['raw_text'] += decoded_line
+                else:
+                    recipe_dict_raw['raw_text'] += decoded_line
+            if re.search('salt', decoded_line):
+                salt = 1
+            elif re.search('main_ingredient', decoded_line):
+                if re.search('plainlist', decoded_line.lower()):
+                    plainlist = 1
+                ingredients = 1
                 recipe_dict_raw['raw_text'] += decoded_line
-    
+            elif re.search('</text>', decoded_line):
+                if ingredients and salt:
+                    recipes_to_parse.put_nowait(recipe_dict_raw)
+                    ingredients = 0
+                mode = 0
+                salt = 0
+            
     print('Done read')
-    recipes_to_parse.put(0)
+    recipes_to_parse.put_nowait(0)
 
 def parse_recipes(wiki_file_name):
     #create queues for shared "arrays"
