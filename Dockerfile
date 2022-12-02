7e996169600e2cf7f70890ee87d9ef450591a2c5
pasavec008 <m.adamovic999@gmail.com>
2022-12-02 11:18:14 +0100

    Spark container setup

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM iisas/hadoop-spark-pig-hive:2.9.2
+
+WORKDIR /vinf_recipes
+
+RUN apt-get update &&\
+    apt-get -y install python3-pip &&\
+     export PYSPARK_PYTHON=python3 &&\
+    export PYTHONIOENCODING=utf8 &&\
+    ln -sf /usr/bin/python3 /usr/bin/python &&\
+    printf "export SPARK_DIST_CLASSPATH=\$(/usr/local/hadoop/bin/hadoop classpath)\n\
+    export SPARK_WORKER_MEMORY=4096m\n\
+    export SPARK_DAEMON_MEMORY=4096m\n\
+    export SPARK_WORKER_INSTANCES=4\n\
+    export SPARK_WORKER_CORES=8" > '/usr/local/spark-2.4.3-bin-without-hadoop-scala-2.12/conf/spark-env.sh' &&\
+    /usr/local/spark-2.4.3-bin-without-hadoop-scala-2.12/sbin/stop-slaves.sh &&\
+    /usr/local/spark-2.4.3-bin-without-hadoop-scala-2.12/sbin/start-slaves.sh
+
+COPY ./requirements.txt .
+RUN pip3 install -r requirements.txt
+
+WORKDIR /usr/local/spark/jars
+RUN wget https://repo1.maven.org/maven2/com/databricks/spark-xml_2.11/0.9.0/spark-xml_2.11-0.9.0.jar
--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from time import time
 from parse_recipes import parse_recipes
 from recommend_recipes import recommend_recipes
 
+#pyspark
+from pyspark import SparkContext, SparkConf 
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+sc = SparkContext('local[8]', 'vinf_recipes')
+spark = SparkSession.builder.master('local[8]').appName('vinf_recipes').getOrCreate()
+
 WIKI_FILE_NAME = '3_big.bz2'
 
 def welcome():
@@ -15,47 +23,59 @@ s -> start recipe recommending
 x -> exit application
 w -> save parsed data to json file\n''')
 
+def parsing_wiki(row):
+    print('cau')
+    return 'cau'
+
 def main():
-    nltk.download('wordnet')
-    nltk.download('omw-1.4')
+    # we need to put file on hadoop first -> with this command in console:
+    # hadoop fs -put /vinf_recipes/1
 
-    recipes = None
-    welcome()
-    while (True):
-        user_input = input('Enter letter: ').lower()
+    f = sc.textFile("/1")
+    df = spark.createDataFrame(f, StringType()).toDF("text")
+    rdd2 = df.rdd.map(lambda row: parsing_wiki(row)).filter(lambda row: row != None)
+    rdd2.saveAsTextFile("./recipes2")
 
-        if user_input == 'r':
-            start = time()
-            recipes = parse_recipes(WIKI_FILE_NAME)
-            print("Parsing recipes from raw wiki data completed in time: ", round(time() - start, 2), ' seconds.')
+    # nltk.download('wordnet')
+    # nltk.download('omw-1.4')
 
-        elif user_input == 'f':
-            parsed_recipes = open('parsed_recipes.json')
-            recipes = json.load(parsed_recipes)
-            print(str(len(recipes)) + ' recipes loaded successfully from parsed json data.\n')
+    # recipes = None
+    # welcome()
+    # while (True):
+    #     user_input = input('Enter letter: ').lower()
 
-        elif user_input == 's':
-            if recipes == None:
-                print('Firstly, you need to read data! (with option "r" or "f")\n')
-                continue
-            else:
-                recommend_recipes(recipes)
-                welcome()
+    #     if user_input == 'r':
+    #         start = time()
+    #         recipes = parse_recipes(WIKI_FILE_NAME)
+    #         print("Parsing recipes from raw wiki data completed in time: ", round(time() - start, 2), ' seconds.')
 
-        elif user_input == 'w':
-            if recipes == None:
-                print('Firstly, you need to read data! (with option "r" or "f")\n')
-                continue
-            with open('parsed_recipes.json', 'w') as parsed_recipes:
-                parsed_recipes.write(json.dumps(recipes))
-                print(str(len(recipes)) + ' recipes saved successfully to json.\n')
+    #     elif user_input == 'f':
+    #         parsed_recipes = open('parsed_recipes.json')
+    #         recipes = json.load(parsed_recipes)
+    #         print(str(len(recipes)) + ' recipes loaded successfully from parsed json data.\n')
 
-        elif user_input == 'x':
-            return
+    #     elif user_input == 's':
+    #         if recipes == None:
+    #             print('Firstly, you need to read data! (with option "r" or "f")\n')
+    #             continue
+    #         else:
+    #             recommend_recipes(recipes)
+    #             welcome()
 
-        else:
-            print('Unknown option!\n')
-            continue
+    #     elif user_input == 'w':
+    #         if recipes == None:
+    #             print('Firstly, you need to read data! (with option "r" or "f")\n')
+    #             continue
+    #         with open('parsed_recipes.json', 'w') as parsed_recipes:
+    #             parsed_recipes.write(json.dumps(recipes))
+    #             print(str(len(recipes)) + ' recipes saved successfully to json.\n')
+
+    #     elif user_input == 'x':
+    #         return
+
+    #     else:
+    #         print('Unknown option!\n')
+    #         continue
 
 if __name__ == '__main__':
     main()
